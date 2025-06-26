3f17e35610bd7bfff7ff1473cf7e60fdcfa450ff
Kalum Gunawardhana <kguna415@gmail.com>
2025-06-26 14:30:00 +0530

    first commit

---
+++ backend/src/main/java/com/internship/portal/StudentInternshipPortalApplication.java
@@ -0,0 +1,11 @@
+package com.internship.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInternshipPortalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StudentInternshipPortalApplication.class, args);
+	}
+}
