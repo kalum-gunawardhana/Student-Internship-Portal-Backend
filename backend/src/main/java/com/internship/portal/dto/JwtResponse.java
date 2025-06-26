5976f816ccad5a3d9d6449b28fc7a5b79a4497fe
Kalum Gunawardhana <kguna415@gmail.com>
2025-06-26 17:00:21 +0530

    commit

--- backend/src/main/java/com/internship/portal/dto/JwtResponse.java
+++ backend/src/main/java/com/internship/portal/dto/JwtResponse.java
@@ -1,5 +1,19 @@
 package com.internship.portal.dto;
 
-public class JwtResponse {
+import com.internship.portal.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private String fullName;
+    private Role role;
 }
